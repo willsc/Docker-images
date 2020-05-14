a3ff2ed76866084754cec89fb22369e16f68fdaf
C Wills <caron.wills@gmail.com>
2020-05-14 19:49:25 +0100

    Removed *.json

--- provider.tf
+++ provider.tf
@@ -1,5 +1,5 @@
 provider "google" {
-credentials = "${file("gcp-jenkins-project-c6215cf91324.json")}"
+credentials = "${file("/Users/cwills/Downloads/gcp-jenkins-project-c6215cf91324.json")}"
 project = "gcp-jenkins-project"
 region = "europe-west2"
 }
